99498717a0b48b375a7492436c2aaaf97256aa2c
Letícia Cardoso <letticardoso@sempreceub.com>
2026-05-15 16:48:16 +0000

    Atualizar configuração do CI para usar npx serve e ajustar baseUrl nos testes do Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    // Aponta para a porta padrão do seu Live Server/Mapeamento local
-    baseUrl: 'http://localhost:5501/FocusFlow',
-    supportFile: false
+    // Porta padrão para o container rodar os testes
+    baseUrl: 'http://localhost:8080',
+    supportFile: false,
+    specPattern: 'cypress/e2e/**/*.spec.js'
   },
 });
